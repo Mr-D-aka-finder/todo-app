c12129a67a4db04f3424086896f9416b81399a40
D <d.hirohata@icloud.com>
2022-08-31 01:48:55 +0900

    フォームの状態管理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 export const App = () => {
+  const [text, setText] = useState("");
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
         <input type="submit" value="add" onSubmit={(e) => e.preventDefault()} />
       </form>
     </div>
